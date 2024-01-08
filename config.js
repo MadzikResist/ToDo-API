c05e0acb1a48e7dc89ff8c9e4832e585f47676f2
Magdalena Szpitalna <madzikresist@gmail.com>
2024-01-08 18:34:49 +0100

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3001,
-    database: process.env.DATABASE || 'mongodb+srv://magdalena:Magdalena1211@todo.0o4ojls.mongodb.net/?retryWrites=true&w=majority'
+    port: process.env.PORT,
+    database: process.env.DATABASE
 };
